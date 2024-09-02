921003401f98b8cc69bea604c26b4d8ece58e66f
Praval <pravalp949@gmail.com>
2024-09-02 19:12:27 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import WeatherDisplay from './WeatherDisplay';
 
-const App = () => {
+function App() {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: "" });
+
+  useEffect(() => {
+    // Update the weather data with the given input
+    setWeather({ temperature: 25, conditions: "Sunny" });
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WeatherDisplay = ({weather}) => {
+  const{temperature,conditions} = weather
+  const temperatureColor = temperature > 20 ? 'red' : 'black';
+  return (
+    <div>
+       <p>Temperature: <span style={{ color: temperatureColor }}>{temperature}</span></p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  )
+}
+
+export default WeatherDisplay
